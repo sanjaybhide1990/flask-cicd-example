25924b5224ebbde06be76669ed692140b78bd4e8
sanjaybhide_clover <sanjay.bhide@cloverventures.in>
2024-01-30 15:12:35 +0530

    Added postgres db connection

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+import uuid,pytz
 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/cars"
+db = SQLAlchemy(app)
+app.app_context().push()
+
+IST = pytz.timezone('Asia/Kolkata')
+# migrate = Migrate(app,db)
+
+def generate_uuid(): 
+    return str(uuid.uuid4())
+
+class ridersModel(db.Model): 
+    __tablename__ = 'riders'
+
+    id = db.Column(db.String(36),primary_key=True,default=generate_uuid,nullable=False)
+    name = db.Column(db.Text(30),nullable=False)
+    team = db.Column(db.Text(30),nullable=False)
+    created_at = db.Column(db.DateTime,nullable=False)
+    updated_at = db.Column(db.DateTime,nullable=False)
+
+    def __init__(self,name,team,created_at,updated_at):
+        self.name = name
+        self.team = team
+        self.created_at = created_at
+        self.updated_at = updated_at
+    
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+
+@app.route('/addRider')
+def add_rider():
+    return 'Test statement!!'
+
 
 @app.route('/test-route')
 def test_route():
